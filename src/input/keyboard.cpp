3f8b648198e5180fd035e15826c00d26655c461f
pgvalle <pgvalle.nogueira@gmail.com>
2023-08-13 13:01:03 -0300

    refactoring keyboard

--- src/input/keyboard.cpp
+++ src/input/keyboard.cpp
@@ -3,21 +3,16 @@
 
 namespace qub3d
 {
-	struct keyboard_data
-	{
-		int keys[350];
-	};
-
-	static keyboard_data keyboard = { { GLFW_RELEASE } };
+	static int keys[350] = { GLFW_RELEASE };
 
 	// IN HEADER
 
 	int get_key(int key)
 	{
-		const int action = keyboard.keys[key];
+		const int action = keys[key];
 		if (action == GLFW_PRESS)
 		{
-			keyboard.keys[key] = GLFW_REPEAT;
+			keys[key] = GLFW_REPEAT;
 		}
 		return action;
 	}
@@ -26,6 +21,6 @@ namespace qub3d
 
 	void key_callback(GLFWwindow* window, int key, int code, int action, int mods)
 	{
-		keyboard.keys[key] = action;
+		keys[key] = action;
 	}
 }
