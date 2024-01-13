ed69e1708b7594ae503fff8a5692e886300e6e8c
pgvalle <pgvalle.nogueira@gmail.com>
2024-01-12 21:04:06 -0300

    added precompiled headers

---
+++ source/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <string>
+#include <vector>
+
+#include <glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include "utils/Logger.h"
